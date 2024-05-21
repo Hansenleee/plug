a101f529f2d7adeaf60eab1f89c8394185c53c1a
libin <libin02@gumingnc.com>
2024-05-21 20:48:48 +0800

    chore: 修改 rollup 打包配置

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import http from 'http';
+import { testtt } from 'test';
 
 const server = http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.write('request successfully proxied!');
   res.end();
 });
+
+console.log(111111, testtt);
 
 server.listen(5050);
---
+++ src/test.ts
@@ -0,0 +1,1 @@
+export const testtt = 1;
