d65348a0a37dd7c8ee1f398a219c63a59122bef4
libin <libin02@gumingnc.com>
2024-05-24 17:18:06 +0800

    feat: 添加 typedi 依赖

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,1 @@
-import http from 'http';
-
-const server = http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('request successfully proxied!');
-  res.end();
-});
-
-console.log(22222, testtt);
-
-server.listen(5050);
+import 'reflect-metadata';
