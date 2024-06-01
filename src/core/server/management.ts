eae0ef35f42c26dccf5c8ab0fbd462fb34759d17
libin <libin02@gumingnc.com>
2024-06-01 11:47:24 +0800

    feat: 搭建 ui 模块

--- src/core/server/management.ts
+++ src/core/server/management.ts
@@ -1,8 +1,10 @@
 import Container, { Service } from 'typedi';
 import Koa from 'koa';
 import Router from '@koa/router';
+import views from '@ladjs/koa-views';
 import { Configuration } from '../../configuration';
 import { Controller } from '../controller';
+import path from 'path';
 
 @Service()
 export class ManagementServer {
@@ -10,10 +12,18 @@ export class ManagementServer {
   private router = new Router();
 
   start() {
+    this.renderView();
     this.registerRouter();
 
     this.app.use(this.router.routes()).use(this.router.allowedMethods());
     this.app.listen(Configuration.MANAGEMENT_PORT);
+  }
+
+  private renderView() {
+    const viewPath = path.join(process.cwd(), 'src/ui/build');
+    const render = views(viewPath, {});
+
+    this.app.use(render);
   }
 
   private registerRouter() {
