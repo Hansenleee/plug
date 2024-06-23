dbc261703ed3fef1444a4d8a23bc0105405693b4
libin_li <libin_li@youzan.com>
2024-06-23 12:32:42 +0800

    chore: 调整打包策略

--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,12 @@ import typescript from 'rollup-plugin-typescript2';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
-import babel from '@rollup/plugin-babel';
+// import babel from '@rollup/plugin-babel';
 import terser from '@rollup/plugin-terser';
+import { visualizer } from 'rollup-plugin-visualizer';
+
+const IS_DEV = process.env.NODE_ENV === 'dev';
+const IS_ANALYZE = !!process.env.BUILD_ANALYZE;
 
 export default {
   external: ['chalk'],
@@ -14,12 +18,20 @@ export default {
   },
   plugins: [
 		typescript(),
-    nodeResolve(),
+    nodeResolve({
+      resolveOnly: module => {
+        return !module.includes('babel');
+      }
+    }),
     commonjs(),
     json(),
-    babel({
-      "presets": ["@babel/preset-typescript"]
-    }),
-    terser()
+    // babel({
+    //   "presets": ["@babel/preset-typescript"]
+    // }),
+    !IS_DEV && terser(),
+    IS_ANALYZE && visualizer({
+      open: true,
+      gzipSize: true
+    })
 	]
 };
