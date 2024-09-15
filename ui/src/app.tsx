8a5d31b0d7d98c58fa5533b670638d0b9b48d87c
libin_li <libin_li@youzan.com>
2024-09-15 17:09:32 +0800

    fix: 处理 ui 层文件大小写问题

---
+++ ui/src/app.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { Layout, Menu, Image, Typography, Space } from 'antd';
+import { SettingOutlined, ApiOutlined } from '@ant-design/icons';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './route';
+import './style.scss';
+
+const { Header, Content } = Layout;
+const ITEMS = [
+  {
+    label: '系统配置',
+    key: 'config',
+    icon: <SettingOutlined />,
+  },
+  {
+    label: '代理设置',
+    key: 'mock',
+    icon: <ApiOutlined />,
+    children: [
+      // {
+      //   label: (
+      //     <a href="/management/mock/base" rel="noopener noreferrer">
+      //       基础配置
+      //     </a>
+      //   ),
+      //   key: 'mockSetting',
+      //   disabled: true,
+      // },
+      {
+        label: (
+          <a href="/management/mock/yapi" rel="noopener noreferrer">
+            mock
+          </a>
+        ),
+        key: 'yapiSetting',
+      },
+    ],
+  },
+];
+
+export const App: React.FC = () => {
+  return (
+    <Layout className="app-container">
+      <Header style={{ display: 'flex', alignItems: 'center', background: '#fff' }}>
+        <Space.Compact className="header-logo" onClick={() => window.location.replace('/management')}>
+          <Image
+            height={58}
+            preview={false}
+            style={{ display: 'block' }}
+            src="https://leebucket01.oss-cn-shanghai.aliyuncs.com/Statics/images/plug-logo-opacity.png"
+          />
+          <Typography.Text className="header-title">Plug Proxy</Typography.Text>
+        </Space.Compact>
+        <Menu
+          mode="horizontal"
+          items={ITEMS}
+          style={{ flex: 1, minWidth: 0, flexDirection: 'row-reverse', border: 0 }}
+        />
+      </Header>
+      <Content>
+        <div className="content-body">
+          <RouterProvider router={router} />
+        </div>
+      </Content>
+    </Layout>
+  );
+};
