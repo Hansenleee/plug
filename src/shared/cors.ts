387db22a2b1fb36e5bfd77ced3ac22632713563c
libin <libin02@gumingnc.com>
2025-04-24 20:36:41 +0800

    fix: 增加跨域请求头

--- src/shared/cors.ts
+++ src/shared/cors.ts
@@ -7,5 +7,7 @@ export const cors = (request: http.IncomingMessage) => {
   return {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Headers': requestHeaderList.join(','),
+    // 跨域时 options 请求有效性，FireFox 上限是 24h = 86400秒
+    'Access-Control-Max-Age': 86400,
   };
 };
